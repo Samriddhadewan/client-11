9374f9f03f71246ece2ecf9cfcdf5c97e78db7e8
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-04-19 19:15:14 +0600

    search functionality addded

--- src/Pages/AllVolunteerPost.jsx
+++ src/Pages/AllVolunteerPost.jsx
@@ -4,18 +4,20 @@ import PostCards from "../Components/PostCards";
 
 const AllVolunteerPost = () => {
   const [posts, setPosts] = useState([]);
-
+  const [search, setSearch] = useState("");
   useEffect(() => {
     const getData = async () => {
       const { data } = await axios.get(
-        `${import.meta.env.VITE_API_URL}/posts`
+        `${import.meta.env.VITE_API_URL}/posts?search=${search}`
       );
       setPosts(data);
     };
     getData();
-  }, []);
+  }, [search]);
 
-  console.log(posts);
+
+  console.log(search);
+
   return (
     <div>
       <div className="min-h-[80vh] max-w-7xl mx-auto my-7 ">
@@ -38,14 +40,16 @@ const AllVolunteerPost = () => {
               <path d="m21 21-4.3-4.3"></path>
             </g>
           </svg>
-          <input type="search" required placeholder="Search Campaigns here" />
+          <input onChange={(e)=> setSearch(e.target.value)} type="search" required placeholder="Search Campaigns here" />
         </label>
         </div>
 
         {/* cards section here  */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
             {
-                posts.map((post, idx)=> <PostCards key={idx} post={post}></PostCards>)
+               posts?.length >0 ?  posts.map((post, idx)=> <PostCards key={idx} post={post}></PostCards>) :<div className="col-span-full text-center text-gray-500 text-xl font-semibold">
+               No campaigns found
+             </div>
             }
         </div>
       </div>
